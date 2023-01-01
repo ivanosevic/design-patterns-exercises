b2bef33964268dbefc2be6b4441147a96d6832fc
garcia <garcia@infox.de>
2023-01-01 17:30:05 +0100

    start exercise 3

---
+++ exercise3-iterators/src/IteratorsExample.java
@@ -0,0 +1,5 @@
+public class IteratorsExample {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ exercise3-iterators/src/ProductCollection.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductCollection {
+  private List<Product> products = new ArrayList<>();
+
+  public void add(Product product) {
+    products.add(product);
+  }
+
+}
